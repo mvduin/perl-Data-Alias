mvduin/perl-Data-Alias

commit 8b78a68c56113e7626c27e3b4649c05e476dd751

Fix localized alias-assignment to previously non-existing hash element
Fixes: https://rt.cpan.org/Ticket/Display.html?id=127717

Alias.xs
@@ -679,23 +679,41 @@ STATIC I32 da_avhv_index(pTHX_ AV *av, SV *key) {
 }
 #endif
 
+#ifndef save_hdelete
+STATIC void save_hdelete(pTHX_ HV *hv, SV *keysv) {
+	STRLEN len;
+	const char *key = SvPV_const(keysv, len);
+	save_delete(hv, savepvn(key, len), SvUTF8(keysv) ? -(I32)len : (I32)len);
+}
+#endif
+
 STATIC OP *DataAlias_pp_helem(pTHX) {
 	dSP;
 	SV *key = POPs;
 	HV *hv = (HV *) POPs;
 	HE *he;
+	bool const localizing = PL_op->op_private & OPpLVAL_INTRO;
+
 	if (SvRMAGICAL(hv) && da_badmagic(aTHX_ (SV *) hv))
 		DIE(aTHX_ DA_TIED_ERR, "put", "into", "hash");
+
 	if (SvTYPE(hv) == SVt_PVHV) {
+		bool existed = TRUE;
+		if (localizing)
+			existed = hv_exists_ent(hv, key, 0);
 		if (!(he = hv_fetch_ent(hv, key, TRUE, 0)))
 			DIE(aTHX_ PL_no_helem, SvPV_nolen(key));
-		if (PL_op->op_private & OPpLVAL_INTRO)
-			save_helem(hv, key, &HeVAL(he));
+		if (localizing) {
+			if (!existed)
+				save_hdelete(hv, key);
+			else
+				save_helem(hv, key, &HeVAL(he));
+		}
 	}
 #if DA_FEATURE_AVHV
 	else if (SvTYPE(hv) == SVt_PVAV && avhv_keys((AV *) hv)) {
 		I32 i = da_avhv_index(aTHX_ (AV *) hv, key);
-		if (PL_op->op_private & OPpLVAL_INTRO)
+		if (localizing)
 			save_aelem((AV *) hv, i, &AvARRAY(hv)[i]);
 		key = (SV *) (Size_t) i;
 	}

t/20_alias_helem.t
@@ -3,7 +3,7 @@
 use strict;
 use warnings qw(FATAL all);
 use lib 'lib';
-use Test::More tests => 38;
+use Test::More tests => 44;
 
 use Data::Alias;
 
@@ -28,6 +28,9 @@ is \$x{0}, \$x{2};
 alias { is \(local $x{0} = $x{1}), \$x{1}; is \$x{0}, \$x{1} };
 is \$x{0}, \$x{2};
 
+alias { is \(local $x{3} = $x{1}), \$x{1}; is \$x{3}, \$x{1} };
+ok !exists $x{3};
+
 is \alias($x{0} = undef), \undef;
 ok !exists $x{0};
 
@@ -36,9 +39,9 @@ ok exists $x{0};
 
 SKIP: {
 no warnings 'deprecated';
-skip "pseudo-hashes not supported anymore", 19 unless eval { [{1,1},1]->{1} };
+skip "pseudo-hashes not supported anymore", 22 unless eval { [{1,1},1]->{1} };
 
-our $y = [{0 => 1, 1 => 2, 2 => 3}];
+our $y = [{0 => 1, 1 => 2, 2 => 3, 3 => 4}];
 
 is \alias($y->{0} = $y->{1}), \$y->{1};
 is \$y->{0}, \$y->{1};
@@ -57,6 +60,9 @@ is \$y->{0}, \$y->{2};
 
 alias { is \(local $y->{0} = $y->{1}), \$y->{1}; is \$y->{0}, \$y->{1} };
 is \$y->{0}, \$y->{2};
+
+alias { is \(local $y->{3} = $y->{1}), \$y->{1}; is \$y->{3}, \$y->{1} };
+ok !exists $y->{3};
 
 is \alias($y->{0} = undef), \undef;
 ok !exists $y->{0};

commit 7cff24a041eec713ccbe4cb436e1a872a67c6d45

fix polyfill of save_hdelete for threaded perl
it lacked the necessary context-passing.

Fixes: 8b78a68c5611 Fix localized alias-assignment to previously non-existing hash element

Alias.xs
@@ -668,11 +668,12 @@ STATIC I32 da_avhv_index(pTHX_ AV *av, SV *key) {
 #endif
 
 #ifndef save_hdelete
-STATIC void save_hdelete(pTHX_ HV *hv, SV *keysv) {
+STATIC void DataAlias_save_hdelete(pTHX_ HV *hv, SV *keysv) {
 	STRLEN len;
 	const char *key = SvPV_const(keysv, len);
 	save_delete(hv, savepvn(key, len), SvUTF8(keysv) ? -(I32)len : (I32)len);
 }
+#define save_hdelete(hv, keysv) DataAlias_save_hdelete(aTHX_ (hv), (keysv))
 #endif
 
 STATIC OP *DataAlias_pp_helem(pTHX) {